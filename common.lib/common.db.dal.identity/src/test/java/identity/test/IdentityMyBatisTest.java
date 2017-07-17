dec3f43c1d2d476d41aa4ce5ddf38ec54c66ed40
jjq <cz_jjq@163.com>
2017-07-17 14:36:48 +0800

    add junit-spring-mybatis support

--- common.lib/common.db.dal.identity/src/test/java/identity/test/IdentityMyBatisTest.java
+++ common.lib/common.db.dal.identity/src/test/java/identity/test/IdentityMyBatisTest.java
@@ -11,8 +11,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-//@RunWith(SpringJUnit4ClassRunner.class)     //表示继承了SpringJUnit4ClassRunner类  
-//@ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})  
+@RunWith(SpringJUnit4ClassRunner.class)     //表示继承了SpringJUnit4ClassRunner类  
+@ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})  
 public class IdentityMyBatisTest {
 
 	protected final Logger logger = LoggerFactory
