ee6b255f8736305268fbf8ef6764143cdf7207de
jjq <cz_jjq@163.com>
2017-07-18 10:08:17 +0800

    add AbstractSecurityController

---
+++ common.lib/common.web.server/src/main/java/common/web/controller/AbstractSecurityController.java
@@ -0,0 +1,47 @@
+package common.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.SpringSecurityMessageSource;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import common.service.utils.AbstractHelperClass;
+
+public abstract class AbstractSecurityController extends AbstractHelperClass {
+	
+	protected UserDetails getCurrentUserDetails(){
+		UserDetails ret = null;
+		// check if user is login
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (!(auth instanceof AnonymousAuthenticationToken)) {
+			ret = (UserDetails) auth.getPrincipal();
+			
+		}
+		return ret;
+	}
+	
+	protected String getCurrentUserName() {
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String username = "";
+		if (principal instanceof UserDetails) {
+			username = ((UserDetails) principal).getUsername();
+		} else {
+			username = principal.toString();
+		}
+		return username;
+	}
+	
+	protected boolean isAuthenticated() {
+		SecurityContext securityContext = SecurityContextHolder.getContext();
+		Authentication authentication = securityContext.getAuthentication();
+		boolean isAuthenticated = !(authentication instanceof AnonymousAuthenticationToken)
+				&& authentication.isAuthenticated();
+		return isAuthenticated;
+	}
+	
+}
