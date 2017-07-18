6db2e28e870641bd380f78571ad0c88f486235eb
jjq <cz_jjq@163.com>
2017-07-18 13:02:49 +0800

    restruct

---
+++ common.lib/common.service.utils/src/main/java/common/service/utils/BindingResultHelper.java
@@ -0,0 +1,43 @@
+package common.service.utils;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+
+import common.service.exception.BindingResultErrorException;
+
+public class BindingResultHelper extends AbstractHelperClass{
+	//由AbstractHelperClass提供的静态类方法支持函数，必须放在子类中
+	protected final static String getStaticClassName(){
+			return new Object() {
+				//静态方法中获取当前类名
+				public String getClassName() {
+					String className = this.getClass().getName();
+					return className.substring(0, className.lastIndexOf('$'));
+				}
+			}.getClassName();
+		}
+		
+	protected final static Logger logger = LoggerFactory
+				.getLogger(getStaticClassName());
+		//------------------static 方法模板定义结束---------------------	
+
+
+	public static void checkValidateResult(BindingResult result) throws BindingResultErrorException{
+		if(result.hasErrors()){
+			List<ObjectError> list = result.getAllErrors();
+			String jsonString=JsonUtil.toJson(list);
+			for(ObjectError objectError:list){
+	            logger.debug(objectError.getDefaultMessage());
+	            logger.debug(objectError.toString());
+	        }
+			throw new BindingResultErrorException(jsonString);
+
+        }
+	}	
+
+}
+
