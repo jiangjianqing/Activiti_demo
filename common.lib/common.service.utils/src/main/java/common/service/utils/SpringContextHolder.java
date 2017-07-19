35c12668859b3d65db470482c9cc8543f56c47e1
jjq <cz_jjq@163.com>
2017-07-19 08:46:06 +0800

    add static-init support

--- common.lib/common.service.utils/src/main/java/common/service/utils/SpringContextHolder.java
+++ common.lib/common.service.utils/src/main/java/common/service/utils/SpringContextHolder.java
@@ -2,6 +2,8 @@ package common.service.utils;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -20,7 +22,35 @@ import org.springframework.web.context.support.WebApplicationContextUtils;
  * 
  */
 public class SpringContextHolder implements ApplicationContextAware {
+	
+	//由AbstractHelperClass提供的静态类方法支持函数，必须放在子类中
+	protected final static String getStaticClassName(){
+			return new Object() {
+				//静态方法中获取当前类名
+				public String getClassName() {
+					String className = this.getClass().getName();
+					return className.substring(0, className.lastIndexOf('$'));
+				}
+			}.getClassName();
+	}
+		
+	protected final static Logger logger = LoggerFactory
+				.getLogger(getStaticClassName());
+		//------------------static 方法模板定义结束---------------------
+		
 	private static ApplicationContext applicationContext;
+	
+	static {
+		try {
+			//通过servletContext.setAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE, this.context);
+			//已经将WebApplicationContext的实例放入ServletContext 中了。
+			
+			//重要：20170719 这里直接直接使用spring提供的contextLoader来获取applicationContext,所以系统中可以不初始化本类了
+			applicationContext = ContextLoader.getCurrentWebApplicationContext();
+		} catch (Exception ex) {
+			logger.error("无法静态初始化WebApplicationContext:"+ex.getMessage());
+		}
+	}
 
 	/**
 	 * 实现ApplicationContextAware接口的context注入函数, 将其存入静态变量.
@@ -74,16 +104,8 @@ public class SpringContextHolder implements ApplicationContextAware {
 
 	private static void checkApplicationContext() {
 		if (applicationContext == null) {
-			
-//通过servletContext.setAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE, this.context);
-			//已经将WebApplicationContext的实例放入ServletContext 中了。
-			
-			//重要：20170719 这里直接直接使用spring提供的contextLoader来获取applicationContext,所以系统中可以不初始化本类了
-			applicationContext = ContextLoader.getCurrentWebApplicationContext();
-		    //WebApplicationContext webApplicationContext = ContextLoader.getCurrentWebApplicationContext();    
-            //ServletContext servletContext = webApplicationContext.getServletContext();  
-			//throw new IllegalStateException(
-			//		"applicaitonContext未注入,请在applicationContext.xml中定义SpringContextHolder");
+			throw new IllegalStateException(
+					"applicaitonContext未注入,请在applicationContext.xml中定义SpringContextHolder");
 		}
 	}
 }
