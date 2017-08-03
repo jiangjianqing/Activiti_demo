33cbce29fefceee61123b1eb68566787db869e43
jjq <cz_jjq@163.com>
2017-08-03 13:17:59 +0800

    fix errorlog's tablename

--- common.lib/common.db.dal.log/src/main/java/common/db/model/log/ErrorLog.java
+++ common.lib/common.db.dal.log/src/main/java/common/db/model/log/ErrorLog.java
@@ -60,7 +60,7 @@ uniqueConstraints = @UniqueConstraint(
 @Entity
 @DynamicInsert //设置为true,表示insert对象的时候,生成动态的insert语句,如果这个字段的值是null就不会加入到insert语句当中.默认false。
 @DynamicUpdate //设置为true,表示update对象的时候,生成动态的update语句,如果这个字段的值是null就不会被加入到update语句中,默认false。
-@Table(name="COMMON_LOG_SESSION")
+@Table(name="COMMON_LOG_ERROR")
 @NamedQueries({
     //@NamedQuery(name="findAll",query="SELECT u FROM User u"),
     //@NamedQuery(name="findUserWithId",query="SELECT u FROM User u WHERE u.id = ?1"),
