492746bde745ec875aed5e01d497c48fd3605cc3
cz_jjq <cz_jjq@163.com>
2017-08-08 06:25:47 +0800

    add common-web

---
+++ common.lib/common.web.server/src/main/java/common/web/controller/advice/ViewControllerExceptionAdvice.java
@@ -0,0 +1,55 @@
+package common.web.controller.advice;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.NativeWebRequest;
+
+import common.service.utils.AbstractHelperClass;
+import common.web.controller.AbstractViewController;
+
+/**
+ * 处理所有视图controller异常
+ * 注意：
+ * 	以下@ExceptionHandler方法处理都禁止标注@ResponseBody  //重要：如果不使用@ResponseBody标注，将返回一个逻辑视图名  
+ * @author jjq
+ *
+ *basePackages用于指定对哪些包里的Controller起作用,无法使用通配符
+ *所以使用assignableTypes来代替（也可以使用annotations 实现同样功能）
+ */
+@ControllerAdvice(assignableTypes = {AbstractViewController.class}) 
+public class ViewControllerExceptionAdvice extends AbstractHelperClass {
+
+	//用于自动绑定前台请求参数到Model中。
+    @InitBinder  
+    public void initBinder(WebDataBinder binder) {  
+    	
+    	//// 此处仅演示忽略request中的参数id
+    	//binder.setDisallowedFields("id");
+    	//用到的场景非常少,目前没有测试过
+    	//System.out.println("============应用到所有@RequestMapping注解方法，在其执行之前初始化数据绑定器"); 
+    }
+    
+    /**
+     * 使用jpa中的异常对象进行数据反馈
+     * @param ex
+     * @return
+     * 
+     * //可以把异常处理器应用到所有控制器，而不是@Controller注解的单个控制器【这个非常重要】
+     */
+    @ExceptionHandler(EntityNotFoundException.class)  
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)  
+	public String processEntityNotFoundException(NativeWebRequest request, EntityNotFoundException ex){
+		//System.out.println("===========应用到所有@RequestMapping注解的方法，在其抛出UnauthenticatedException异常时执行");  
+    	//return "viewName"; 
+    	
+    	return "error500";
+	}
+
+}
+
---
+++ common.lib/common.web.server/src/main/java/common/web/controller/rest/UserController.java
@@ -0,0 +1,131 @@
+package common.web.controller.rest;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.persistence.EntityNotFoundException;
+import javax.validation.Valid;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import common.db.base.exception.DaoException;
+import common.db.base.exception.OutOfPageRangeException;
+import common.db.base.page.PageObject;
+import common.db.model.identity.User;
+import common.db.repository.jpa.identity.UserDAO;
+import common.security.AuthenticationUser;
+import common.security.PasswordEncoderAssist;
+import common.security.SimpleUserDetailsService;
+import common.service.utils.BindingResultHelper;
+import common.web.controller.AbstractRestController;
+import common.web.model.WrappedResponseBody;
+
+/**
+ * 如类级别的映射为 @RequestMapping(value="/narrow", produces="text/html")，
+ * 方法级别的为@RequestMapping(produces="application/xml")，
+ * 此时方法级别的映射将覆盖类级别的，因此请求头headers=“Accept:application/xml”是成功的(produces)，而“text/html”将报406错误码，表示不支持的请求媒体类型。
+ * 只有生产者/消费者 模式 是 覆盖，其他的使用方法是继承，如headers、params等都是继承。
+ * 
+ * 组合使用是“或”的关系
+ * @RequestMapping(produces={"text/html", "application/json"}) ：将匹配“Accept:text/html”或“Accept:application/json”。
+ * 
+ * 问题
+ * 消费的数据，如JSON数据、XML数据都是由我们读取请求的InputStream并根据需要自己转换为相应的模型数据；
+ * 生产的数据，如JSON数据、XML数据都是由我们自己先把模型数据转换为json/xml等数据，然后输出响应流。
+ * Spring提供了一组注解（@RequestBody、@ResponseBody）和一组转换类（HttpMessageConverter）来完成
+ * 
+ * @RequestMapping注解方法支持的返回值类型
+ * ModelAndView对象，with the model implicitly enriched with command objects and the results of @ModelAttributes annotated reference data accessor methods.（恕我实在翻译不出 TAT）。
+ * Model对象，with the view name implicitly determined through a RequestToViewNameTranslator and the model implicitly enriched with command objects and the results of @ModelAttribute annotated reference data accessor methods.
+ * Map对象，for exposing a model, with the view name implicitly determined through a RequestToViewNameTranslator and the model implicitly enriched with command objects and the results of @ModelAttribute annotated reference data accessor methods.
+ * View对象，with the model implicitly determined through command objects and @ModelAttribute annotated reference data accessor methods. The handler method may also programmatically enrich the model by declaring a Model argument (see above).
+ * String，value that is interpreted as the logical view name, with the model implicitly determined through command objects and @ModelAttribute annotated reference data accessor methods. The handler method may also programmatically enrich the model by declaring a Model argument (see above).
+ * void，if the method handles the response itself (by writing the response content directly, declaring an argument of type ServletResponse / HttpServletResponse for that purpose) or if the view name is supposed to be implicitly determined through a RequestToViewNameTranslator (not declaring a response argument in the handler method signature).
+ * @ResponseBody，If the method is annotated with @ResponseBody, the return type is written to the response HTTP body. The return value will be converted to the declared method argument type using HttpMessageConverters. See Section 16.3.3.5, “Mapping the response body with the @ResponseBody annotation”.
+ * HttpEntity或者ResponseEntity，HttpEntity<?>或者ResponseEntity<?>对象可以取到Servlet的response的HTTP头信息（headers）和内容（contents）。这个实体（entity body）可以通过使用HttpMessageConverter类被转成Response流。See Section 16.3.3.6, “Using HttpEntity<?>”.
+ * Any other return type is considered to be a single model attribute to be exposed to the view, using the attribute name specified through @ModelAttribute at the method level (or the default attribute name based on the return type class name). The model is implicitly enriched with command objects and the results of @ModelAttribute annotated reference data accessor methods.
+
+ * @author cz_jjq
+ *
+ *
+ *重要：
+ *
+ *spring MVC Controller默认是单例的，单例的原因有二：
+1、为了性能。
+2、不需要多例。
+ *
+ *Controller最佳实践：
+1、不要在controller中定义成员变量。
+2、万一必须要定义一个非静态成员变量时候，则通过注解@Scope("prototype")，将其设置为多例模式。
+ */
+
+@Controller	
+@RequestMapping("/rest/identity/user")
+@SessionAttributes(types = {AuthenticationUser.class,/*String.class*/},value={"currentUser","session.message"})//将符合types或者vlaue的ModelMap对象 存入Session
+//放到Session属性列表中，以便这个属性可以跨请求访问
+public class UserController extends AbstractRestController{
+	
+	@Resource
+	private PasswordEncoderAssist passwordEncoderAssist;
+	
+	@Resource
+	private UserDAO userDao;
+	// private Map<String, Info> model = Collections.synchronizedMap(new
+	// HashMap<String, Info>());
+	/**
+	 * 调试方法，用于观察所有数据
+	 * @return
+	 * @throws DaoException
+	 */
+	@RequestMapping(value="all",method=RequestMethod.GET)
+	@ResponseBody
+	public List<User> getAll() throws DaoException{
+		return userDao.getList();
+	}
+	
+	@RequestMapping(value={"" , "/"},method=RequestMethod.GET)
+	@ResponseBody
+	public WrappedResponseBody getPageList(@RequestParam(defaultValue="1" , name = "${naming.requestParam.page}") int page) throws OutOfPageRangeException, DaoException{
+		return new WrappedResponseBody(userDao.getPageList(page));
+	}
+	
+	//public ResponseEntity<String> createMembership(@RequestParam String userId
+	//		,@RequestParam(required=false) String[] groupIds){
+	
+	@RequestMapping(value={"" , "/"},method=RequestMethod.POST)
+	@ResponseBody
+	public WrappedResponseBody create(@Valid @RequestBody User user, BindingResult result /*其他参数必须在result后面*/) throws DaoException{
+		BindingResultHelper.checkValidateResult(result);
+		passwordEncoderAssist.encodeNewUserPassword(user);//对密码进行加密处理
+    	userDao.create(user);
+        return new WrappedResponseBody(user);  //验证成功
+	}
+	
+	@RequestMapping(value={"/{id}"},method={RequestMethod.GET})
+	@ResponseBody
+	public WrappedResponseBody findById(@PathVariable Long id) throws DaoException, EntityNotFoundException{
+		User user=userDao.findByKey(id);
+		if(user==null){
+			throw new EntityNotFoundException(id.toString());
+		}
+		return new WrappedResponseBody(user);
+	}
+	
+	@RequestMapping(value={"/{id}"},method={RequestMethod.PUT})
+	@ResponseBody
+	public WrappedResponseBody update(@PathVariable Long id,@Valid @RequestBody User user,BindingResult result ) throws DaoException{
+		BindingResultHelper.checkValidateResult(result);
+		user.setId(id);
+		return new WrappedResponseBody(userDao.update(user));
+	}
+	
+	@RequestMapping(value={"/{id}"},method={RequestMethod.DELETE})
+	@ResponseBody
+	public WrappedResponseBody  delete(@PathVariable Long id) throws DaoException{
+		userDao.deleteByKey(id);
+		return new WrappedResponseBody("");
+	}
+
+}
