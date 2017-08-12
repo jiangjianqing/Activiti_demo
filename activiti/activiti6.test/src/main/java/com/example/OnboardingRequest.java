9494c084cdb02628093f682577fca07cc49bdd7d
jjq <cz_jjq@163.com>
2017-08-12 14:23:57 +0800

    init

--- activiti/activiti6.test/src/main/java/com/example/OnboardingRequest.java
+++ activiti/activiti6.test/src/main/java/com/example/OnboardingRequest.java
@@ -2,7 +2,10 @@ package com.example;
 
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.RepositoryService;
 import org.activiti.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.ProcessDefinition;
 
 public class OnboardingRequest {
 	public static void main(String[] args) {
@@ -12,9 +15,19 @@ public class OnboardingRequest {
 			      .setJdbcPassword("")
 			      .setJdbcDriver("org.h2.Driver")
 			      .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
-			    ProcessEngine processEngine = cfg.buildProcessEngine();
-			    String pName = processEngine.getName();
-			    String ver = ProcessEngine.VERSION;
-			    System.out.println("ProcessEngine [" + pName + "] Version: [" + ver + "]");
+		ProcessEngine processEngine = cfg.buildProcessEngine();
+		String pName = processEngine.getName();
+		String ver = ProcessEngine.VERSION;
+		System.out.println("ProcessEngine [" + pName + "] Version: [" + ver + "]");
+		
+		RepositoryService repositoryService = processEngine.getRepositoryService();
+	    Deployment deployment = repositoryService.createDeployment()
+	        .addClasspathResource("diagrams/onboarding.bpmn20.xml").deploy();
+	    ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
+	        .deploymentId(deployment.getId()).singleResult();
+	    System.out.println(
+	        "Found process definition [" 
+	            + processDefinition.getName() + "] with id [" 
+	            + processDefinition.getId() + "]");
 	}
 }
